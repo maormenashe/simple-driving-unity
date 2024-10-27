8444a11ab00c5d791e6c3bc66f851c9cce212daf
MaorM <maormenashe2@gmailcom>
2024-10-27 21:18:02 +0200

    Using UI Pointers For Steering The Car

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -4,11 +4,21 @@ public class Car : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     [SerializeField] private float speedGain = 0.2f;
+    [SerializeField] private float turnSpeed = 200f;
 
+    private int steer;
 
     void Update()
     {
         speed += speedGain * Time.deltaTime;
+
+        transform.Rotate(0f, steer * turnSpeed * Time.deltaTime, 0f);
+
         transform.Translate(speed * Time.deltaTime * Vector3.forward);
+    }
+
+    public void Steer(int value)
+    {
+        steer = value;
     }
 }
